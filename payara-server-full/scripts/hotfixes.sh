7174c4548b38822dc4830ca4be5fff15930afb6a
Rune Rønneseth <rune.ronneseth@kred.no>
2023-06-16 12:12:09 +0200

    wip

--- payara-server-full/scripts/hotfixes.sh
+++ payara-server-full/scripts/hotfixes.sh
@@ -1,12 +1,15 @@
 #!/bin/env bash
 set -e
 
-# ISSUE:
-#   > https://github.com/payara/Payara/issues/2267 & https://github.com/payara/Payara/issues/5192
-# FIXED:
-#   > https://github.com/payara/Payara/pull/5193 (Payara 6.x)
+# ISSUE: https://github.com/payara/Payara/issues/2267 & https://github.com/payara/Payara/issues/5192
+# FIXED: https://github.com/payara/Payara/pull/5193 (Payara 6.x)
 if [ "$X_HOTFIX_1_ENABLED" ]; then
-  echo "[INFO] Adding hostname to hosts-file"
+  echo "[INFO] Adding hostname HOSTALIASES"
   echo 127.0.0.1 `cat /etc/hostname` | tee -a $HOME_DIR/.hosts
   export HOSTALIASES="$HOME_DIR/.hosts"
+fi
+
+if [ "$X_HOTFIX_2_ENABLED" ]; then
+  echo "[INFO] Adding hostname to hosts-file (requires root)"
+  echo 127.0.0.1 `cat /etc/hostname` | tee -a /etc/hosts
 fi
